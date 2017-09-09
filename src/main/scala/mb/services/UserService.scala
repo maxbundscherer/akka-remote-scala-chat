8d299084c9b87f81975bb7c1f2ef8351dcb73306
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2017-09-09 19:58:53 +0200

    add user service

---
+++ src/main/scala/mb/services/UserService.scala
@@ -0,0 +1,37 @@
+package mb.services
+
+import mb.utils.DatabaseDriver
+import mb.models.UserEntity
+import mb.models.db.UserTableEntity
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
+
+class UserService(databaseService: DatabaseService) extends UserTableEntity with DatabaseDriver {
+
+  import databaseDriver._
+  import databaseService._
+
+  //TODO: Exception handling and async programming
+
+  /**
+    * create user
+    * @param userEntity UserEntity (without hashedPassword)
+    */
+  def create(userEntity: UserEntity): Unit = Await.result( db.run( userQuery += userEntity.withHashedPassword ), Duration.Inf )
+
+  /**
+    * check authData and return userId
+    * @param username String
+    * @param password String
+    * @return Option(None) if authData incorrect / Option(Long) with userId if authData correct
+    */
+  def checkAuthDataAndGetId(username: String, password: String): Option[Long] = {
+
+    val entity = Await.result(db.run( userQuery.filter(_.username === username).result.headOption ), Duration.Inf)
+
+    if(entity.isDefined && entity.get.checkPassword(password)) Option(entity.get.id.get)
+    else None
+  }
+
+}
